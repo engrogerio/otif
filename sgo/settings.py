9bd7e26d193b5c10cd387bc2646107cf74c39532
rogerio <eng.rogerio@gmail.com>
2017-02-24 16:00:14 -0300

    db oracle

--- sgo/settings.py
+++ sgo/settings.py
@@ -108,9 +108,16 @@ WSGI_APPLICATION = 'sgo.wsgi.application'
 DATABASES = {
 
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'localhost',
+        'USER': 'sgo',
+        'PASSWORD': 'sgo',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
