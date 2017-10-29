b794ee5f610534f6bc486c7a9477de698dcda096
rogerio <eng.rogerio@gmail.com>
2017-10-29 11:44:58 -0200

    issue #51

--- cliente/admin.py
+++ cliente/admin.py
@@ -1,8 +1,9 @@
 # -*- encoding: utf-8 -*-
 from django.contrib import admin
 from django import forms
-from cliente.models import Cliente
+from cliente.models import Cliente, Grupo, PreCarregamento
 from sgo.admin import SgoModelAdmin
+
 # Register your models here.
 
 class LimitePorClienteAdminForm(forms.ModelForm):
@@ -17,15 +18,22 @@ class LimitePorClienteAdmin(SgoModelAdmin):
     #inlines = [GradeInline,]
     #exclude = ('item',)
     verbose_name = ("Limites Atraso Cliente")
-    list_display = ('nm_ab_cli', 'hr_lim_carga','hr_lim_lib', ) #'cliente_limite', )
-    readonly_fields = ('nm_ab_cli',)
+    list_display = ( 'nm_ab_cli', 'hr_lim_carga', 'hr_lim_lib', ) #'cliente_limite', )
+    # readonly_fields = ('nm_ab_cli',)
     fieldsets = (
         (None, {'fields':(
-                          ('nm_ab_cli',('hr_lim_carga','hr_lim_lib',)))
+                          (('nm_ab_cli', ),('hr_lim_carga', 'hr_lim_lib',)))
                 }),
     )
     #list_filter = ('nm_ab_cli',)
-    search_fields = ['nm_ab_cli',]
+    search_fields = ['nm_ab_cli', ]
 
+class GrupoAdmin(SgoModelAdmin):
+    filter_horizontal = ('cliente',)
+
+class PreCarregamentoAdmin(SgoModelAdmin):
+    filter_horizontal = ('grupo',)
 
 admin.site.register(Cliente, LimitePorClienteAdmin)
+admin.site.register(Grupo, GrupoAdmin)
+admin.site.register(PreCarregamento, PreCarregamentoAdmin)
--- cliente/models.py
+++ cliente/models.py
@@ -1,12 +1,26 @@
 # -*- encoding: utf-8 -*-
+
 from django.db import models
 from sgo.models import OtifModel
 # Create your models here.
 
+
+class Grupo(OtifModel):
+    nm_grupo = models.IntegerField('Código do Grupo', null='true', blank='true',)
+    cliente = models.ManyToManyField('Cliente', null='true', blank='true',)
+
+    class Meta:
+        verbose_name_plural = "Grupos de Cliente"
+        
+    def __unicode__(self):
+        return str(self.nm_grupo)
+
+
 class Cliente(OtifModel):
     nm_ab_cli = models.CharField('Código do cliente', max_length=24, unique='true', )
     ds_classe_cli = models.CharField('Canal do cliente', max_length=30, null='true', blank='true', )
-    hr_lim_carga = models.TimeField('Limite de atraso na chegada do caminhão (hs)', null='true', blank='true', )
+    hr_lim_carga = models.TimeField('Limite de atraso na chegada do caminhão (hs)', null='true', 
+        blank='true', )
     hr_lim_lib = models.TimeField('Limite de atraso de liberação (hs)', null='true', blank='true', )
 
     class Meta:
@@ -14,3 +28,11 @@ class Cliente(OtifModel):
 
     def __unicode__(self):
         return  self.nm_ab_cli
+
+
+class PreCarregamento(OtifModel):
+    grupo = models.ManyToManyField(Grupo, null='true', blank='true',)
+
+    def __str__(self):
+        return  "Grupos com Pré-Carregamento"
+    
